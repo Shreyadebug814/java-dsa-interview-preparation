04720355c7bfb0523e41cf7083f6901007b74c83
Shreyas ks <ksshreyas077@gmail.com>
2026-07-05 00:31:27 +0530

    Solved Array Q1 using loop - Find Largest Element

--- src/arrays/FindLargest.java
+++ src/arrays/FindLargest.java
@@ -1,19 +1,19 @@
 package arrays;
 
 public class FindLargest {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int largest = findLargest(arr);
-        System.out.println("The largest number in the array is: " + largest);
-    }
+     public static void main(String[] args){
+//     Input array
+         int[] arr = {10,5,20,8,15};
+//         Assume first element is the largest
+         int largest = arr[0];
+//         Traverse the array  from index 1
+         for(int i = 1; i<arr.length;i++){
+//             compare current element with largest
+             if(arr[i] > largest){
+                 largest = arr[i];
 
-    public static int findLargest(int[] arr) {
-        int largest = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            }
-        }
-        return largest;
-    }
+             }
+         }
+         System.out.println("Largest element:"+ largest);
+     }
 }
