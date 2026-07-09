cf5243565885d5acb2b0f7cf5635c2e5c74edd26
Shreyas ks <ksshreyas077@gmail.com>
2026-07-09 14:22:11 +0530

    Solved Array Q1 using loop and Streams - FindLargestUsingLoopAndStreams

---
+++ src/arrays/FindLargestUsingLoopAndStreams.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class FindLargestUsingLoopAndStreams {
+    public static void main(String[] args){
+        int[] arr = {10,5,20,8,15};
+        int largestUsingLoop = findLargestUsingLoop(arr);
+        System.out.println("Largest element using for loop :" + largestUsingLoop);
+        int largestUsingStreams = findLargestUsingStreams(arr);
+        System.out.println("Largest element using streams :" + largestUsingStreams);
+    }
+
+    private static  int findLargestUsingLoop(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+            int largest = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > largest) {
+                    largest = arr[i];
+
+                }
+            }
+            return largest;
+    }
+
+    private static int findLargestUsingStreams(int[] arr){
+       int largestElement = Arrays.stream(arr).max().getAsInt();
+       return largestElement;
+    }
+}
+
+//"I made the method static because it doesn't depend on any object-specific data or instance variables.
+// It only processes the input array passed as a parameter. Since main() is also static,
+// making the helper method static allows me to call it directly without creating an unnecessary object."
